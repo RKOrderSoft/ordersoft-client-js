9fa7a18e716d11e38bd3d3273f5840d1f0db3fdd
jason_xh <jaesonxh@gmail.com>
2018-02-19 12:51:50 +1100

    First commit
    added constant, xhr notes and main module thing

---
+++ clientSideLibrary.js
@@ -0,0 +1,23 @@
+const IP = // IP
+
+/* xml http request
+
+xhr.open("POST", url, true);
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+xhr.send(someStuff);
+
+That code would post someStuff to url. Just make sure that when you create your 
+XMLHttpRequest object, it will be cross-browser compatible. There are endless examples out there of how to do that.
+
+/*
+
+*/
+
+module.exports = {
+
+	//
+	sendRequest: function() {
+		return "HELLO";
+
+
+};
